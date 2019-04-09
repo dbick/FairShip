fef6f26420277cdc4661cf6b54fbf92795297270
Stefan Bieschke <Dinge_passieren@web.de>
2019-04-09 12:22:17 +0200

    Changed a wrong call, now from within the same module

--- charmdet/DtAlignment/utils/utils.py
+++ charmdet/DtAlignment/utils/utils.py
@@ -169,7 +169,7 @@ def calculate_residuals(track,dtmodules,module_residuals):
         det_id = raw_measurement.getDetId()
         rt_dist = raw_measurement.getRawHitCoords()[6] * u.mm #rt distance stored here
         # 2.) Parse detector id to module
-        module_id = DtAlignment.utils.parse_det_id(det_id)
+        module_id = parse_det_id(det_id)
         module = dtmodules[module_id['module']]
         # 3.) Find correct drift tube in module
         for j in range(len(module.get_tubes())):
