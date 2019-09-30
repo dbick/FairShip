9722b4bab2de634f3fdc6339fdba69ae6defe470
Stefan Bieschke <Dinge_passieren@web.de>
2019-09-30 08:41:38 +0200

    Typos fixed

--- charmdet/MillepedeCaller.cxx
+++ charmdet/MillepedeCaller.cxx
@@ -165,11 +165,11 @@ vector<gbl::GblPoint> MillepedeCaller::list_hits(const genfit::Track* track) con
 
 		cout << "Hit #" << it - jacobians_with_arclen.begin() "/" << jacobians_with_arclen.end() - jacobians_with_arclen.begin() << endl;
 		cout << "Jacobian before heap deletion" << endl;
-		result.back().getgetP2pJacobian().Print();
+		result.back().getP2pJacobian().Print();
 		//TODO check if this is allowed. Only, if GblPoint stores a copy of the jacobian
 		delete jacobian;
 		cout << "Jacobian before heap deletion" << endl;
-		result.back().getgetP2pJacobian().Print();
+		result.back().getP2pJacobian().Print();
 	}
 
 	return result;
