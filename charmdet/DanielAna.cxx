281707e539081592e7d05986890be0f5305bf97f
Daniel Bick <daniel.bick@desy.de>
2020-04-23 14:31:31 +0200

    tested RT relation stuff, implemented draw method

--- charmdet/DanielAna.cxx
+++ charmdet/DanielAna.cxx
@@ -1,13 +1,18 @@
 #include "DanielAna.h"
 #include <iostream>
 #include "MufluxSpectrometerHit.h"
+#include "ScintillatorHit.h"
 #include "MufluxSpectrometer.h"
 #include "MufluxSpectrometerRTRelation.h"
-#include "FairRunSim.h" 
 
 #include "TTreeReaderArray.h"
 #include "TVector3.h"
 #include "TH1.h"
+#include "TROOT.h"
+#include "TStyle.h"
+#include "TCanvas.h"
+#include "TEllipse.h"
+#include "TLine.h"
 
 void helloDaniel(){
   std::cout << "Hello Daniel" << std::endl;
@@ -95,10 +100,10 @@ void dtAnaChain(TTreeReader *t){
       //std::cout << std::endl;
       if(hit->GetTimeOverThreshold()>40&&!(hit->GetTimeOverThreshold()>167.1999&&hit->GetTimeOverThreshold()<=167.2)){
 	HDriftTimes->Fill(hit->GetDigi());
-	HToT->Fill(hit->GetTimeOverThreshold());        
+
       }
 
-
+	HToT->Fill(hit->GetTimeOverThreshold());        
       
       
     }
@@ -127,13 +132,182 @@ void dtAnaChain(TTreeReader *t){
   HRTRelation->Draw();
   */
   
-  //  MufluxSpectrometerRTRelation* RTRel;
+  
+  //MufluxSpectrometerRTRelation* RTRel;
+  //  TH1D *HDriftTimes = new TH1D("HDriftTimes","Drift Time Spectrum",3000,-500,2500);
 
-  MufluxSpectrometerRTRelation* RTRel = new MufluxSpectrometerRTRelation("RTRel","RT Relation",3000,-500,2500);
+  //MufluxSpectrometerRTRelation* RTRel = new MufluxSpectrometerRTRelation("RTRel","RT Relation",3000,-500,2500);
 
-  RTRel->InitRTRelation(HDriftTimes);
+ MufluxSpectrometerRTRelation* RTRel = new MufluxSpectrometerRTRelation(*HDriftTimes);
+  
+ //RTRel->InitRTRelation(*HDriftTimes);
   std::cout << RTRel->GetRadius(100) << std::endl;;
   //RTRel->GetEntries();
+    
   
+}
+
+
+
+
+TH1D *FilterDTSpectrum(TTreeReader *t){
+  
+  TTreeReaderArray <MufluxSpectrometerHit> Digi_MufluxSpectrometerHits(*t, "Digi_MufluxSpectrometerHits");
+  TTreeReaderArray <MufluxSpectrometerHit> Digi_LateMufluxSpectrometerHits(*t, "Digi_LateMufluxSpectrometerHits");
+  TTreeReaderArray <ScintillatorHit> Digi_Triggers(*t, "Digi_Triggers");
+  TTreeReaderArray <ScintillatorHit> Digi_MasterTrigger(*t, "Digi_MasterTrigger");
+  TTreeReaderArray <ScintillatorHit> Digi_Scintillators(*t, "Digi_Scintillators");
+  
+  
+  int event=0;
+  TH1D *HDriftTimes = new TH1D("HDriftTimes","Drift Time Spectrum",3000,-500,2500);
+  TH1D *HToT = new TH1D("HToT","Time Over Threshold",600,0,600);
+  
+  
+  while (t->Next()) {
+    
+    int n =  Digi_MufluxSpectrometerHits.GetSize();
+    int late_n =  Digi_LateMufluxSpectrometerHits.GetSize();
+    int trigger_n =  Digi_Triggers.GetSize();
+    int master_n =  Digi_MasterTrigger.GetSize();
+    int scint_n =  Digi_Scintillators.GetSize();
+    
+    if(event==71832){
+    for (int i = 0; i < n; ++i) {
+      //std::cout << Digi_MufluxSpectrometerHits[i].GetDetectorID() << " " ;
+      MufluxSpectrometerHit* hit = &(Digi_MufluxSpectrometerHits[i]);
+      std::cout << "ID: " << hit->GetDetectorID() << " FLAGS " << hit->GetFlags() << " Time " << hit->GetDigi() << " (" << hit->GetTimeOverThreshold() << ") ";
+      for(int j=0;j<late_n;j++){
+	MufluxSpectrometerHit* latehit = &(Digi_LateMufluxSpectrometerHits[j]);
+	if(hit->GetDetectorID()==latehit->GetDetectorID()){
+	  std::cout << "   " << latehit->GetDigi() << " (" << latehit->GetTimeOverThreshold() << ") ";
+	}
+      }
+      std::cout << std::endl;
+    } 
+    for (int i = 0; i < trigger_n; ++i) {
+      ScintillatorHit* thit = &(Digi_Triggers[i]);
+      std::cout << "Trigger ID: " << thit->GetDetectorID() << " FLAGS " << thit->GetFlags() << " Time " << thit->GetDigi() << " (" << thit->GetTimeOverThreshold() << ") " << std::endl;
+    }
+
+    for (int i = 0; i < scint_n; ++i) {
+      ScintillatorHit* shit = &(Digi_Scintillators[i]);
+      std::cout << "Scint ID: " << shit->GetDetectorID() << " FLAGS " << shit->GetFlags() << " Time " << shit->GetDigi() << " (" << shit->GetTimeOverThreshold() << ") " << std::endl;
+    }
+
+    DrawDTEvent(Digi_MufluxSpectrometerHits);   
+    }
+    //break;
+    event++;
+  }
+  
+  
+  return HDriftTimes;
+}
+
+
+  
+void DrawDTEvent(TTreeReaderArray <MufluxSpectrometerHit> &Digi_MufluxSpectrometerHits){
+ 
+    
+  gStyle->SetLineScalePS(1);
+  TCanvas *disp = new TCanvas("disp","Event Display",1600,480);
+  disp->Range(0,-120,800,120);
+
+  TCanvas *zoomdisp = new TCanvas("zoomdisp","Event Display Zoom",960,768);
+  zoomdisp->Range(0,-72,180,72);
+  
+  Int_t DetectorID;
+
+  MufluxSpectrometer* mySpectrometer= new MufluxSpectrometer();
+  TVector3 *vtop = new TVector3();
+  TVector3 *vbot = new TVector3();
+
+  //Draw Detector Geometry
+
+  //stations 1 and 2 have two views with one module each
+  for(int station=1;station<=2;station++){
+    for(int vnb=0;vnb<2;vnb++){
+      for(int pnb=0;pnb<2;pnb++){
+	for(int lnb=0;lnb<2;lnb++){
+	  for(int wire=1;wire<=12;wire++){
+	    DetectorID=station*10000000+vnb*1000000+pnb*100000+lnb*10000+2000+wire;
+	    mySpectrometer->TubeEndPoints(DetectorID, *vbot, *vtop);
+	    //std::cout << DetectorID << " " << vtop->x() << " " << vtop->y() << " " << vtop->z() << std::endl;
+	    double scale=1;
+	    if(station+vnb==2)scale=1./cos(60*3.14159/180);//scale stereo views
+	    TEllipse *el = new TEllipse((vtop->z()+vbot->z())/2,scale*(vtop->x()+vbot->x())/2,2.,2.);
+	    el->Draw();
+	    zoomdisp->cd();
+	    el->Draw();
+	    disp->cd();
+	  }
+	}
+      }
+    }
+  }
+
+
+   //stations 3 and 4 have one view with four modules
+  for(int station=3;station<=4;station++){
+    for(int vnb=0;vnb<1;vnb++){
+      for(int pnb=0;pnb<2;pnb++){
+	for(int lnb=0;lnb<2;lnb++){
+	  for(int wire=1;wire<=48;wire++){
+	    DetectorID=station*10000000+vnb*1000000+pnb*100000+lnb*10000+2000+wire;
+	    mySpectrometer->TubeEndPoints(DetectorID, *vbot, *vtop);
+	    //std::cout << DetectorID << " " << vtop->x() << " " << vtop->y() << " " << vtop->z() << std::endl;
+	    TEllipse *el = new TEllipse((vtop->z()+vbot->z())/2,(vtop->x()+vbot->x())/2,2.,2.);
+	    el->Draw();
+	  }
+	}
+      }
+    }
+  }
+
+
+  //Draw Hits
+
+
+
+ 
+
+  int n =  Digi_MufluxSpectrometerHits.GetSize();
+  for(int i=0;i<n;i++){
+    MufluxSpectrometerHit* hit = &(Digi_MufluxSpectrometerHits[i]);
+    DetectorID = hit->GetDetectorID();
+
+    int station=DetectorID/10000000;
+    int vnb=(DetectorID%10000000)/1000000;
+    
+    //hit->MufluxSpectrometerEndPoints(*vbot,*vtop);
+    //std::cout << "Method 1: " << vtop->x() << " " << vtop->y() << " " << vtop->z() << std::endl;
+
+    mySpectrometer->TubeEndPoints(DetectorID, *vbot, *vtop);
+
+    //std::cout << "Method 2: " << vtop->x() << " " << vtop->y() << " " << vtop->z() << std::endl;
+    double scale=1;
+    if(station+vnb==2)scale=1./cos(60*3.14159/180);//scale stereo views
+    TEllipse *hitel = new TEllipse((vtop->z()+vbot->z())/2,scale*(vtop->x()+vbot->x())/2,2.,2.);
+    hitel->SetFillColor(2);
+    if(station+vnb==2)hitel->SetFillColor(3+vnb);
+    hitel->Draw();
+    zoomdisp->cd();
+    hitel->Draw();
+    disp->cd();
+
+    
+    if(station<3){
+      TLine *tube = new TLine(
+			      -vtop->x()+400,
+			      vtop->y(),
+			      -vbot->x()+400,
+			      vbot->y()
+			      );
+      tube->SetLineColor(2);
+      if(station+vnb==2)tube->SetLineColor(3+vnb);
+      tube->Draw();
+    }
+  }
   
 }
--- charmdet/DanielAna.h
+++ charmdet/DanielAna.h
@@ -4,13 +4,14 @@
 #include "TROOT.h"
 #include "TTree.h"
 #include "TTreeReader.h"
+#include "TTreeReaderArray.h"
+#include "TH1D.h"
+#include "MufluxSpectrometerHit.h"
 
 void helloDaniel();
 void dtAna(TTree *TData);
 void dtAnaChain(TTreeReader *t);
-
-
-
-
-
+TH1D *FilterDTSpectrum(TTreeReader *t);
+//void DrawEvent(MufluxSpectrometerHit* hit);
+void DrawDTEvent(TTreeReaderArray <MufluxSpectrometerHit> &Digi_MufluxSpectrometerHits);
 #endif
