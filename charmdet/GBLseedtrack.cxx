fc91614fe94962c428fd47d709d01395779b4483
Stefan Bieschke <Dinge_passieren@web.de>
2020-04-29 15:37:16 +0200

    Initial implementation of a class used as GBL seed

---
+++ charmdet/GBLseedtrack.cxx
@@ -0,0 +1,73 @@
+/*
+ * GBLseedtrack.cpp
+ *
+ *  Created on: 29.04.2020
+ *      Author: bieschke
+ */
+
+#include "GBLseedtrack.h"
+
+using std::vector;
+using std::pair;
+
+GBL_seed_track::GBL_seed_track(const genfit::Track& track)
+{
+	vector<genfit::TrackPoint*> points = track.getPoints();
+	size_t n_points = points.size();
+
+	m_hits.resize(n_points);
+
+	for(size_t i = 0; i < n_points; ++i)
+	{
+		genfit::TrackPoint* point = points[i];
+		genfit::AbsMeasurement* raw_measurement = point->getRawMeasurement();
+		int det_id = raw_measurement->getDetId();
+		double drift_radius = raw_measurement->getRawHitCoords()[6];
+		pair<int,double> hit_info = std::make_pair(det_id, drift_radius);
+
+		m_hits[i] = hit_info;
+	}
+
+	m_position = track.getFittedState().getPos();
+	m_direction = track.getFittedState().getMom();
+}
+
+GBL_seed_track::GBL_seed_track::~GBL_seed_track()
+{
+	// TODO Auto-generated destructor stub
+}
+
+const size_t GBL_seed_track::get_number_of_hits() const
+{
+	return m_hits.size();
+}
+
+
+const TVector3& GBL_seed_track::get_position() const
+{
+	return m_position;
+}
+
+const TVector3& GBL_seed_track::get_direction() const
+{
+	return m_direction;
+}
+
+const vector<pair<int,double>>& GBL_seed_track::get_hits() const
+{
+	return m_hits;
+}
+
+
+
+const vector<int> GBL_seed_track::get_hit_detIDs() const
+{
+	vector<int> result(m_hits.size());
+
+	for(size_t i = 0; i < result.size(); ++i)
+	{
+		result[i] = m_hits[i].first;
+	}
+
+	return result;
+}
---
+++ charmdet/GBLseedtrack.h
@@ -0,0 +1,35 @@
+/*
+ * GBLseedtrack.h
+ *
+ *  Created on: 29.04.2020
+ *      Author: bieschke
+ */
+
+#include "Track.h"
+#include "TVector3.h"
+#include <vector>
+#ifndef CHARMDET_GBLSEEDTRACK_H_
+#define CHARMDET_GBLSEEDTRACK_H_
+
+class GBL_seed_track
+{
+public:
+	GBL_seed_track(const genfit::Track& track);
+	virtual ~GBL_seed_track();
+
+	inline const size_t get_number_of_hits() const;
+	inline const TVector3& get_position() const;
+	inline const TVector3& get_direction() const;
+
+	inline const std::vector<std::pair<int,double>>& get_hits() const;
+
+	const std::vector<int> get_hit_detIDs() const;
+
+private:
+	TVector3 m_position;
+	TVector3 m_direction;
+
+	std::vector<std::pair<int,double>> m_hits;
+};
+
+#endif /* CHARMDET_GBLSEEDTRACK_H_ */
