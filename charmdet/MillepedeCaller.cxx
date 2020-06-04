137886b653a5d6174cb8b49a46577f68f4975fec
Stefan Bieschke <Dinge_passieren@web.de>
2020-06-04 12:01:06 +0200

    Direction of correction is now printed to console

--- charmdet/MillepedeCaller.cxx
+++ charmdet/MillepedeCaller.cxx
@@ -180,10 +180,11 @@ std::vector<gbl::GblPoint> MillepedeCaller::list_hits(const GBL_seed_track* trac
 		m_survey.TubeEndPointsSurvey(point.first, vtop, vbot);
 		if (pede_corrections)
 		{
-			cout << "Applying" << endl;
 			vector<int> labels_for_tube = calc_labels(MODULE, point.first);
 			double correction_x = (*pede_corrections)[labels_for_tube[0]];
 			double correction_z = (*pede_corrections)[labels_for_tube[2]];
+			cout << "Correcting: " << point.first << " x +" << correction_x << " z +" << correction_z << endl;
+
 			vbot[0] = vbot[0] + correction_x;
 			vtop[0] = vtop[0] + correction_x;
 			vbot[2] = vbot[2] + correction_z;
