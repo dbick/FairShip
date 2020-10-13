0b4ea55e056c766745db2295fba894d402b6a7cd
Daniel Bick <daniel.bick@desy.de>
2020-10-13 13:08:44 +0200

    fixxed missing declaration in header

--- charmdet/MufluxSpectrometerDTSurvey.h
+++ charmdet/MufluxSpectrometerDTSurvey.h
@@ -69,7 +69,8 @@ class MufluxSpectrometerDTSurvey{
   DTSurveyPoints DTSurveyDistanceToRefTube(Int_t DetectorID);
   TVector3 DTStaggering(Int_t DetectorID);
   void TubeEndPointsSurvey(Int_t DetectorID, TVector3 &vtop, TVector3 &vbot) const;
-
+  TubeEndPoints TubeEndPointsSurvey(Int_t DetectorID);
+  
   Double_t DTSurveyStereoAngle(Int_t DetectorID);
   Double_t DTSurveyTubeLength(Int_t DetectorID);
   TVector3 AtY(Int_t DetectorID, Double_t y);
